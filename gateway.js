974a168667f087d0f0c099d571a6f8ed1d58d029
yellowemp <silverswine21@gmail.com>
2021-02-06 21:20:07 +0900

    Push BackEnd Files

---
+++ gateway.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const crud = require('./connection.js');
+
+const app = express();
+
+app.get('/search/:name', (req, res) => {
+  crud.search(req.params.name).then(res => console.log(res));
+  return res.send('completed');
+});
+
+app.get('/add/:name', (req, res) => {
+  crud.add(req.params.name);
+  return res.send('completed');
+});
+
+app.get('/update/:oldname/:newname', (req, res) => {
+  crud.update(req.params.oldname,req.params.newname,);
+  return res.send('completed');
+});
+
+app.get('/delete/:name', (req, res) => {
+  crud.delete(req.params.name);
+  return res.send('completed');
+});
+
+app.listen('3000', () =>
+  console.log(`Example app listening on port 3000}!`),
+);
